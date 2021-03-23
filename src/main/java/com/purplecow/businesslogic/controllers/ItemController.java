e32b4d59656961c1e72c20c08394f637afe62537
Gazelle79 <fboydjr@gmail.com>
2021-03-23 01:04:39 -0400

    Changing ItemController so that it implements REST functionality.

--- src/main/java/com/purplecow/businesslogic/controllers/ItemController.java
+++ src/main/java/com/purplecow/businesslogic/controllers/ItemController.java
@@ -1,31 +1,42 @@
 package com.purplecow.businesslogic.controllers;
 
 import com.purplecow.businesslogic.classes.Item;
+import com.purplecow.businesslogic.classes.ItemCollection;
+import com.purplecow.businesslogic.classes.ItemDataAccess;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.*;
 
+/*
+* This class implements the functionality for REST using
+* HTTP GET, POST, PUT, etc.
+* */
 
 @RestController
+@RequestMapping(path = "/item")
 public class ItemController
 {
-    private ArrayList<Item> theseItems = new ArrayList<Item>();
+    private ItemDataAccess dataAccess = new ItemDataAccess();
 
-    @RequestMapping("/")
-    public String index()
-    {
-        String hello = "\n\n Greetings from Spring Boot! \n\n";
-        return hello;
-    }
 
     /*
     Get all the items in the list.
     */
+    @GetMapping(path = "/", produces = "application/json")
     public ArrayList<Item> getItems()
     {
-        return theseItems;
+        return this.dataAccess.getItems();
     }
 
     /*
@@ -33,7 +44,7 @@ public class ItemController
     */
     public void setItems(ArrayList<Item> itemsToSet)
     {
-        this.theseItems = itemsToSet;
+        this.dataAccess.setItems(itemsToSet);
     }
 
     /*
@@ -41,7 +52,7 @@ public class ItemController
     */
     public void deleteItems()
     {
-        this.theseItems.clear();
+        this.dataAccess.deleteItems();
     }
 
 
@@ -51,7 +62,8 @@ public class ItemController
     public Item getItem(UUID itemId)
     {
         Item itemToReturn = null;
-        for(Item thisItem : theseItems)
+        ArrayList<Item> availableItems = this.dataAccess.getItems();
+        for(Item thisItem : availableItems)
         {
             if(thisItem.getId().equals(itemId))
             {
@@ -68,7 +80,8 @@ public class ItemController
     public boolean updateItem(UUID itemId, String newItemName)
     {
         boolean wasItemUpdated = false;
-        for(Item thisItem : theseItems)
+        ArrayList<Item> availableItems = this.dataAccess.getItems();
+        for(Item thisItem : availableItems)
         {
             if(thisItem.getId().equals(itemId))
             {
@@ -86,11 +99,12 @@ public class ItemController
     public boolean deleteItem(UUID itemId)
     {
         boolean wasItemDeleted = false;
-        for(Item thisItem : theseItems)
+        ArrayList<Item> availableItems = this.dataAccess.getItems();
+        for(Item thisItem : availableItems)
         {
             if(thisItem.getId().equals(itemId))
             {
-                theseItems.remove(thisItem);
+                availableItems.remove(thisItem);
                 wasItemDeleted = true;
             }
             // wasItemDeleted stays false if item is never found.
